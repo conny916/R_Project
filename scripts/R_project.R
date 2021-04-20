c34edca8972f1bf54c05dab996c6d811f5574120
conny916 <constantin.zier@gmail.com>
2021-04-20 15:15:44 +0200

    latest r script

--- scripts/R_project.R
+++ scripts/R_project.R
@@ -6,7 +6,6 @@ library(tidyverse)
 ## READ IN DATA AND ORGANIZE ####
 #Read in data
 data1 = read.csv(file = 'data/0001_day_1_cond_1234_test.csv')
-select(imgID)
 data2 = read.csv(file = 'data/0001_day_2_cond_1234_test.csv') %>%
   select(response_oldnew, response_outcome, tendencyCorrect_oldnew, imgID)
 
