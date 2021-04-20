c8161c764a659f14ba3c5b901848af1f96104841
conny916 <constantin.zier@gmail.com>
2021-04-20 15:09:45 +0200

    Latest Update

--- scripts/R_project.R
+++ scripts/R_project.R
@@ -1,12 +1,21 @@
 ## LOAD PACKAGES ####
 library(dplyr)
 library(ggplot2)
+library(tidyverse)
 
 ## READ IN DATA AND ORGANIZE ####
 #Read in data
-read.csv(file = 'data/0001_day_1_cond_1234_test.csv')
+data1 = read.csv(file = 'data/0001_day_1_cond_1234_test.csv')
+select(imgID)
+data2 = read.csv(file = 'data/0001_day_2_cond_1234_test.csv') %>%
+  select(response_oldnew, response_outcome, tendencyCorrect_oldnew, imgID)
+
 #Look at data
 dim(data)
 head(data)
 tail(data)
 xtabs(~category, data)
+
+## MERGE DATA ####
+left_join(data1, data2, by="imgID")
+
